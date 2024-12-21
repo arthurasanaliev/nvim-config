050633ae46eb339e0b504734856ee29ef20e3008
Arthur Asanaliev <arthis1707@gmail.com>
2024-12-22 02:20:30 +0600

    added github theme

--- lua/arthis/options.lua
+++ lua/arthis/options.lua
@@ -14,8 +14,8 @@ vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 10
 
-vim.o.background = "dark"
-vim.cmd("colorscheme nord")
+vim.o.background = "light"
+vim.cmd("colorscheme github_light")
 
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
